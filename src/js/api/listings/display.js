bc85fb917360180ddd9cd97d2f27cc46b60c3d68
Berrinj <69366646+Berrinj@users.noreply.github.com>
2024-09-11 21:49:36 +0200

    clened up commented out code

--- src/js/api/listings/display.js
+++ src/js/api/listings/display.js
@@ -3,12 +3,9 @@ import { authFetch } from "../authFetch.js";
 
 const bids = "_bids=true";
 const seller = "_seller=true";
-// const listings = "_listings=true";
-// const wins = "_wins=true";
 
 // /**
 //  * Fetches all listings from the API
-//  * @param {string} name username of the profile to fetch
 //  * @returns {promise} result of the get
 //  * @throws {Error} If the name parameter is empty or an error occurs during the process.
 //  */
@@ -25,19 +22,3 @@ export async function displayListings() {
     throw new Error("Failed to display listings");
   }
 }
-// export async function displayProfile(name) {
-//   try {
-//     if (!name) {
-//       throw new Error("get requires a profile name");
-//     }
-//     const displayProfileURL = `${API_URL_PROFILE}/${name}?${listings}&${bids}&${wins}`;
-//     console.log(displayProfileURL);
-//     const response = await authFetch(displayProfileURL);
-//     if (response.ok) {
-//       return await response.json();
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     throw new Error("Failed to display profile");
-//   }
-// }
